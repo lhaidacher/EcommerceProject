136d8f6abc8de35bfd99d9427474bacb4db0193a
Lukas Haidacher <lukas.haidacher@beraingpoint.com>
2025-02-12 09:51:00 +0100

    inital commit incl. base structure and executable services

---
+++ services/order-cache-service/src/main/java/at/fhk/wes/OrderCacheApplication.java
@@ -0,0 +1,11 @@
+package at.fhk.wes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderCacheApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderCacheApplication.class, args);
+    }
+}
